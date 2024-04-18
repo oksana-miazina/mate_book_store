3d49e0ced80502fafb993ee41dd4fde2b4de0cf4
Oksana Miazina <oksana.myazina@gmail.com>
2024-04-18 19:59:55 +0300

    fix: add name for books table

--- src/main/java/mate/academy/bookstore/model/Book.java
+++ src/main/java/mate/academy/bookstore/model/Book.java
@@ -5,10 +5,12 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import java.math.BigDecimal;
 import lombok.Data;
 
 @Entity
+@Table(name = "books")
 @Data
 public class Book {
     @Id
@@ -21,6 +23,7 @@ public class Book {
     private String author;
     @Column(nullable = false, unique = true)
     private String isbn;
+
     private BigDecimal price;
     private String description;
     @Column(name = "cover_image")
