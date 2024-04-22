ac0ca0b07a5fdf95a5af2269ab82be0f0bc40e42
Oksana Miazina <oksana.myazina@gmail.com>
2024-04-22 20:06:28 +0300

    feat: add PUT method for books

--- src/main/java/mate/academy/bookstore/controller/BookController.java
+++ src/main/java/mate/academy/bookstore/controller/BookController.java
@@ -4,12 +4,13 @@ import jakarta.validation.Valid;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import mate.academy.bookstore.dto.BookDto;
-import mate.academy.bookstore.dto.CreateBookRequestDto;
+import mate.academy.bookstore.dto.BookRequestDto;
 import mate.academy.bookstore.service.BookService;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -33,7 +34,13 @@ public class BookController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public BookDto createBook(@Valid @RequestBody CreateBookRequestDto bookDto) {
+    public BookDto createBook(@Valid @RequestBody BookRequestDto bookDto) {
         return bookService.save(bookDto);
+    }
+
+    @PutMapping("/{id}")
+    public BookDto updateBookById(@PathVariable Long id,
+                                  @RequestBody BookRequestDto bookRequestDto) {
+        return bookService.updateBookById(id, bookRequestDto);
     }
 }
--- src/main/java/mate/academy/bookstore/service/BookService.java
+++ src/main/java/mate/academy/bookstore/service/BookService.java
@@ -2,12 +2,14 @@ package mate.academy.bookstore.service;
 
 import java.util.List;
 import mate.academy.bookstore.dto.BookDto;
-import mate.academy.bookstore.dto.CreateBookRequestDto;
+import mate.academy.bookstore.dto.BookRequestDto;
 
 public interface BookService {
-    BookDto save(CreateBookRequestDto bookDto);
+    BookDto save(BookRequestDto bookDto);
 
     List<BookDto> findAll();
 
     BookDto getById(Long id);
+
+    BookDto updateBookById(Long id, BookRequestDto bookRequestDto);
 }
