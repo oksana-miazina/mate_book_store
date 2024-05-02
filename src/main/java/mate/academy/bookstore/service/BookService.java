26e152528ca223057ffcae15d56e6ee3e47e8736
Oksana Miazina <oksana.myazina@gmail.com>
2024-05-02 12:39:16 +0300

    feat: add books pagination

--- src/main/java/mate/academy/bookstore/service/BookService.java
+++ src/main/java/mate/academy/bookstore/service/BookService.java
@@ -3,11 +3,12 @@ package mate.academy.bookstore.service;
 import java.util.List;
 import mate.academy.bookstore.dto.BookDto;
 import mate.academy.bookstore.dto.BookRequestDto;
+import org.springframework.data.domain.Pageable;
 
 public interface BookService {
     BookDto save(BookRequestDto bookDto);
 
-    List<BookDto> findAll();
+    List<BookDto> findAll(Pageable pageable);
 
     BookDto getById(Long id);
 
