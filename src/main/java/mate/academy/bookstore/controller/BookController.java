969f118cb63745f885edfa1c3a127b003af6acf4
Oksana Miazina <oksana.myazina@gmail.com>
2024-05-02 17:45:29 +0300

    docs: add swagger

--- src/main/java/mate/academy/bookstore/controller/BookController.java
+++ src/main/java/mate/academy/bookstore/controller/BookController.java
@@ -1,5 +1,9 @@
 package mate.academy.bookstore.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import java.util.List;
 import lombok.AllArgsConstructor;
