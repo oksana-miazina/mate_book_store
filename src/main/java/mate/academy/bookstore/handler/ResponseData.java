11f86ef3aea6935f4816118c2ddb0419e51246de
Oksana Miazina <oksana.myazina@gmail.com>
2024-05-03 12:15:03 +0300

    refactor: rename ExceptionHandler and make ResponseData abstract

--- src/main/java/mate/academy/bookstore/handler/ResponseData.java
+++ src/main/java/mate/academy/bookstore/handler/ResponseData.java
@@ -5,7 +5,7 @@ import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
 @Getter
-public class ResponseData {
+public abstract class ResponseData {
     private boolean success;
 
     private int status;
