a550736aec435cd3c1c43b4708c99bfd2806667a
Oksana Miazina <oksana.myazina@gmail.com>
2024-05-10 17:16:54 +0300

    fix: add Override and Objects.equals

--- src/main/java/mate/academy/bookstore/validator/FieldMatchValidator.java
+++ src/main/java/mate/academy/bookstore/validator/FieldMatchValidator.java
@@ -2,27 +2,25 @@ package mate.academy.bookstore.validator;
 
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
+import java.util.Objects;
 import org.springframework.beans.BeanWrapperImpl;
 
 public class FieldMatchValidator implements ConstraintValidator<FieldMatch, Object> {
     private String field;
     private String fieldMatch;
 
+    @Override
     public void initialize(FieldMatch constraintAnnotation) {
         this.field = constraintAnnotation.field();
         this.fieldMatch = constraintAnnotation.fieldMatch();
     }
 
+    @Override
     public boolean isValid(Object value, ConstraintValidatorContext context) {
         Object fieldValue = new BeanWrapperImpl(value)
                 .getPropertyValue(field);
         Object fieldMatchValue = new BeanWrapperImpl(value)
                 .getPropertyValue(fieldMatch);
-        
-        if (fieldValue != null) {
-            return fieldValue.equals(fieldMatchValue);
-        } else {
-            return fieldMatchValue == null;
-        }
+        return Objects.equals(fieldValue, fieldMatchValue);
     }
 }
