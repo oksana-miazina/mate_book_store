930cfc5a32a2f0f47a86871025dcefed6f323443
Oksana Miazina <oksana.myazina@gmail.com>
2024-05-14 09:58:44 +0300

    feat: add Security

--- src/main/java/mate/academy/bookstore/service/impl/UserServiceImpl.java
+++ src/main/java/mate/academy/bookstore/service/impl/UserServiceImpl.java
@@ -5,18 +5,23 @@ import mate.academy.bookstore.dto.UserRegistrationRequestDto;
 import mate.academy.bookstore.dto.UserResponseDto;
 import mate.academy.bookstore.exception.RegistrationException;
 import mate.academy.bookstore.mapper.UserMapper;
+import mate.academy.bookstore.model.Role;
 import mate.academy.bookstore.model.User;
+import mate.academy.bookstore.repository.RoleRepository;
 import mate.academy.bookstore.repository.UserRepository;
 import mate.academy.bookstore.service.LocaleService;
 import mate.academy.bookstore.service.UserService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @AllArgsConstructor
 public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
     private final UserMapper userMapper;
     private final LocaleService localeService;
+    private final PasswordEncoder encoder;
 
     @Override
     public UserResponseDto register(UserRegistrationRequestDto requestDto)
@@ -25,6 +30,8 @@ public class UserServiceImpl implements UserService {
             throw new RegistrationException(localeService.getMessage("exception.exists.user"));
         }
         User user = userMapper.toModel(requestDto);
+        user.setPassword(encoder.encode(user.getPassword()));
+        user.getRoles().add(roleRepository.getByName(Role.RoleName.USER));
         User savedUser = userRepository.save(user);
         return userMapper.toUserResponseDto(savedUser);
     }
