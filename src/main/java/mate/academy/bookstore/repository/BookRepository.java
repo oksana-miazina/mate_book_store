c421e8a7724498d568471971b8c40c223ef83e5f
Oksana Miazina <oksana.myazina@gmail.com>
2024-05-20 13:51:32 +0300

    refactor: rename book repository methods

--- src/main/java/mate/academy/bookstore/repository/BookRepository.java
+++ src/main/java/mate/academy/bookstore/repository/BookRepository.java
@@ -5,11 +5,9 @@ import java.util.Optional;
 import mate.academy.bookstore.model.Book;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
     Optional<Book> findByIsbn(String isbn);
 
-    @Query("FROM Book b JOIN b.categories c WHERE c.id = :categoryId")
-    List<Book> findAllByCategoryId(Long categoryId, Pageable pageable);
+    List<Book> findAllByCategoriesId(Long categoryId, Pageable pageable);
 }
