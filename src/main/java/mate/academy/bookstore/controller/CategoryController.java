10d6f4783e3fc88789cb6e62348ba33e85a67dc9
Oksana Miazina <oksana.myazina@gmail.com>
2024-05-20 14:17:00 +0300

    docs: describe Category controller

--- src/main/java/mate/academy/bookstore/controller/CategoryController.java
+++ src/main/java/mate/academy/bookstore/controller/CategoryController.java
@@ -1,11 +1,18 @@
 package mate.academy.bookstore.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import mate.academy.bookstore.dto.BookDtoWithoutCategoryIds;
 import mate.academy.bookstore.dto.CategoryDto;
 import mate.academy.bookstore.dto.CategoryRequestDto;
+import mate.academy.bookstore.response.ErrorResponse;
 import mate.academy.bookstore.response.ResponseHandler;
 import mate.academy.bookstore.response.SuccessResponse;
 import mate.academy.bookstore.service.BookService;
@@ -23,6 +30,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Category Management", description = "Endpoints for managing categories")
 @RestController
 @RequestMapping("/api/categories")
 @RequiredArgsConstructor
@@ -33,6 +41,12 @@ public class CategoryController {
     @PostMapping
     @PreAuthorize("hasRole('ADMIN')")
     @ResponseStatus(HttpStatus.CREATED)
+    @Operation(summary = "Create a new category", description = "Create a new category")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", useReturnTypeSchema = true),
+            @ApiResponse(responseCode = "409", description = "Category with such name already exists",
+                    content = { @Content(schema = @Schema(implementation = ErrorResponse.class)) }),
+    })
     public SuccessResponse<CategoryDto> createCategory(
             @Valid @RequestBody CategoryRequestDto categoryDto) {
         return ResponseHandler.getSuccessResponse(
@@ -41,12 +55,19 @@ public class CategoryController {
     }
 
     @GetMapping
+    @Operation(summary = "Get all categories", description = "Get all categories")
     public SuccessResponse<List<CategoryDto>> getAll(Pageable pageable) {
         return ResponseHandler.getSuccessResponse(
                 categoryService.findAll(pageable));
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Get category by id", description = "Get category by id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", useReturnTypeSchema = true),
+            @ApiResponse(responseCode = "404", content =
+                    { @Content(schema = @Schema(implementation = ErrorResponse.class)) }),
+    })
     public SuccessResponse<CategoryDto> getCategoryById(@PathVariable Long id) {
         return ResponseHandler.getSuccessResponse(
                 categoryService.getById(id));
@@ -54,6 +75,14 @@ public class CategoryController {
 
     @PutMapping("/{id}")
     @PreAuthorize("hasRole('ADMIN')")
+    @Operation(summary = "Update an existing category", description = "Update an existing category")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", useReturnTypeSchema = true),
+            @ApiResponse(responseCode = "409",
+                    description = "Category with such name already exists "
+                            + "(possible if new name differs from old one)",
+                    content = { @Content(schema = @Schema(implementation = ErrorResponse.class)) }),
+    })
     public SuccessResponse<CategoryDto> updateCategory(
             @PathVariable Long id, @Valid @RequestBody CategoryRequestDto categoryDto) {
         return ResponseHandler.getSuccessResponse(
@@ -63,11 +92,18 @@ public class CategoryController {
     @DeleteMapping("/{id}")
     @PreAuthorize("hasRole('ADMIN')")
     @ResponseStatus(HttpStatus.NO_CONTENT)
+    @Operation(summary = "Delete category by id", description = "Delete category by id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", useReturnTypeSchema = true),
+            @ApiResponse(responseCode = "404", content =
+                    { @Content(schema = @Schema(implementation = ErrorResponse.class)) }),
+    })
     public void deleteCategory(@PathVariable Long id) {
         categoryService.deleteById(id);
     }
 
     @GetMapping("/{id}/books")
+    @Operation(summary = "Get all books by category id", description = "Get all books in specific category")
     public SuccessResponse<List<BookDtoWithoutCategoryIds>> getBooksByCategoryId(
             @PathVariable Long id, Pageable pageable) {
         return ResponseHandler.getSuccessResponse(
