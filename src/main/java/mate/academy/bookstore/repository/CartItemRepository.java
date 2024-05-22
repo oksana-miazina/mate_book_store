ada5086aca3c31f7cacd689362429156672c1d20
Oksana Miazina <oksana.myazina@gmail.com>
2024-05-22 19:59:25 +0300

    style: checkstyle

--- src/main/java/mate/academy/bookstore/repository/CartItemRepository.java
+++ src/main/java/mate/academy/bookstore/repository/CartItemRepository.java
@@ -1,11 +1,10 @@
 package mate.academy.bookstore.repository;
 
+import java.util.List;
+import java.util.Set;
 import mate.academy.bookstore.model.CartItem;
 import mate.academy.bookstore.model.CartItemKey;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
-import java.util.Set;
 
 public interface CartItemRepository extends JpaRepository<CartItem, CartItemKey> {
     Set<CartItem> getById_Book_IdIn(List<Long> ids);
